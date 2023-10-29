b3bf82ec91e1e8916574f3377f8cd0a6e055cb36
Sandeep <sandeepchopra7@gmail.com>
2023-10-29 19:07:19 -0400

    add users schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,11 @@
-import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  primaryKey,
+  timestamp,
+  uniqueIndex,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
 
 export const applicaton = pgTable("applications", {
   id: uuid("id").primaryKey().defaultRandom(),
@@ -6,3 +13,22 @@ export const applicaton = pgTable("applications", {
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("update_at").defaultNow().notNull(),
 });
+
+export const users = pgTable(
+  "users",
+  {
+    id: uuid("id").defaultRandom().notNull(),
+    email: varchar("email", { length: 256 }).notNull(),
+    name: varchar("name", { length: 256 }).notNull(),
+    applicatonId: uuid("applicationId").references(() => applicaton.id),
+    password: varchar("password", { length: 256 }).notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("update_at").defaultNow().notNull(),
+  },
+  (users) => {
+    return {
+      cpk: primaryKey(users.email, users.applicatonId),
+      idIndex: uniqueIndex("users_id_index").on(users.id),
+    };
+  }
+);
