6a500b7d21a23cd889a02f494d39d69e08a83db9
Sandeep <sandeepchopra7@gmail.com>
2023-10-30 15:02:38 -0400

    ensure unique permission for the given user

--- src/modules/users/users.services.ts
+++ src/modules/users/users.services.ts
@@ -58,7 +58,15 @@ export async function getUserByEmail({
   // AND userToRoles.applicationId = users.applicationId
 
   const result = await db
-    .select()
+    .select({
+      id: users.id,
+      email: users.email,
+      name: users.name,
+      applicationId: users.applicationId,
+      roleId: roles.id,
+      password: users.password,
+      permissions: roles.permissions,
+    })
     .from(users)
     .where(and(eq(users.email, email), eq(users.applicationId, applicationId)))
     .leftJoin(
@@ -78,5 +86,27 @@ export async function getUserByEmail({
     return null;
   }
 
-  return result;
+  const user = result.reduce((acc, curr) => {
+    if (!acc.id) {
+      return {
+        ...curr,
+        permissions: new Set(curr.permissions),
+      };
+    }
+
+    if (!curr.permissions) {
+      return acc;
+    }
+
+    for (const permission of curr.permissions) {
+      acc.permissions.add(permission);
+    }
+
+    return acc;
+  }, {} as Omit<(typeof result)[number], "permissions"> & { permissions: Set<string> });
+
+  return {
+    ...user,
+    permissions: Array.from(user.permissions),
+  };
 }
