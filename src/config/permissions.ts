70ba6c4763952834ee5e17f06bbfb40eed2dd3e5
Sandeep <sandeepchopra7@gmail.com>
2023-10-30 09:48:45 -0400

    add permissions

---
+++ src/config/permissions.ts
@@ -0,0 +1,21 @@
+export const ALL_PERMISSIONS = [
+  "users:roles:write", // allowed to add a role to a user
+  "users:roles:delete", //allowed to remove a role from a user
+  "orders:write",
+  "orders:read",
+] as const;
+
+export const PERMISSIONS = ALL_PERMISSIONS.reduce((acc, permission) => {
+  acc[permission] = permission;
+  return acc;
+}, {} as Record<(typeof ALL_PERMISSIONS)[number], (typeof ALL_PERMISSIONS)[number]>);
+
+export const USER_ROLE = [
+  PERMISSIONS["orders:read"],
+  PERMISSIONS["orders:write"],
+];
+
+export const SYSTEM_ROLES = {
+  SUPER_ADMIN: "SUPER_ADMIN",
+  APPLICATION_USER: "APPLICATION_USER",
+};
